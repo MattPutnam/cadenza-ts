27ba83a754ef16d505e353a5c3f1a0d7e399dee8
Matt Putnam <matt.s.putnam@gmail.com>
2021-09-09 23:38:08 -0400

    stories structure

--- src/app/components/button/button.stories.tsx
+++ src/app/components/button/button.stories.tsx
@@ -3,20 +3,29 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Button } from '.';
+import { StoryWrapper } from '../../../storybook-components';
 
 const onClick = () => {};
 
 storiesOf('Components / Button', module).add('Button', () => (
   <>
-    <Button onClick={onClick}>Regular</Button>
-    <Button large onClick={onClick}>
-      Large
-    </Button>
-    <Button disabled onClick={onClick}>
-      Disabled
-    </Button>
-    <Button large disabled onClick={onClick}>
-      Large Disabled
-    </Button>
+    <StoryWrapper title="Regular">
+      <Button onClick={onClick}>Button</Button>
+    </StoryWrapper>
+    <StoryWrapper title="Large">
+      <Button large onClick={onClick}>
+        Button
+      </Button>
+    </StoryWrapper>
+    <StoryWrapper title="Disabled">
+      <Button disabled onClick={onClick}>
+        Button
+      </Button>
+    </StoryWrapper>
+    <StoryWrapper title="Large Disabled">
+      <Button large disabled onClick={onClick}>
+        Button
+      </Button>
+    </StoryWrapper>
   </>
 ));
--- src/app/components/keyboard/keyboard.stories.tsx
+++ src/app/components/keyboard/keyboard.stories.tsx
@@ -16,12 +16,52 @@ const keyboard = {
   id: 0
 };
 
-storiesOf('Components / Keyboard', module).add('Keyboard', () => {
+storiesOf('Components / Keyboard', module).add('onKeyClick', () => {
   const [key, setKey] = useState<number | undefined>(undefined);
 
   return (
     <StoryWrapper title="Keyboard with onKeyClick">
       <Keyboard keyboard={keyboard} onKeyClick={setKey} />
+      <Row>{`Key clicked: ${key ? Midi.midiNoteNumberToName(key) : 'none'}`}</Row>
+    </StoryWrapper>
+  );
+});
+
+storiesOf('Components / Keyboard', module).add('onRangeDrag', () => {
+  const [range, setRange] = useState<number[] | undefined>(undefined);
+
+  return (
+    <StoryWrapper title="Keyboard with onRangeDrag">
+      <Keyboard keyboard={keyboard} onRangeDrag={setRange} />
+      <Row>{`Range dragged: ${
+        range ? `${Midi.midiNoteNumberToName(range[0])}-${Midi.midiNoteNumberToName(range[1])}` : 'none'
+      }`}</Row>
+    </StoryWrapper>
+  );
+});
+
+storiesOf('Components / Keyboard', module).add('highlightKeys', () => {
+  return (
+    <StoryWrapper title="Keyboard with highlightKeys">
+      <Keyboard keyboard={keyboard} highlightKeys={[60, 70, 80]} />
+    </StoryWrapper>
+  );
+});
+
+storiesOf('Components / Keyboard', module).add('lightHighlightKeys', () => {
+  return (
+    <StoryWrapper title="Keyboard with lightHighlightKeys">
+      <Keyboard keyboard={keyboard} lightHighlightKeys={[60, 70, 80]} />
+    </StoryWrapper>
+  );
+});
+
+storiesOf('Components / Keyboard', module).add('onKeyClick and highlightKeys', () => {
+  const [key, setKey] = useState<number | undefined>(undefined);
+
+  return (
+    <StoryWrapper title="Keyboard with onKeyClick and highlightKeys">
+      <Keyboard keyboard={keyboard} onKeyClick={setKey} highlightKeys={[60, 72, 84]} />
       <Row>{`Key clicked: ${key ? Midi.midiNoteNumberToName(key) : 'none'}`}</Row>
     </StoryWrapper>
   );
