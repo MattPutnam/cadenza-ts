ba9250052d9056be476b98440a0519fa4c1020dc
Matt Putnam <matt.s.putnam@gmail.com>
2021-09-13 19:48:58 -0400

    Better note name display

--- src/midi/notes.ts
+++ src/midi/notes.ts
@@ -1,7 +1,13 @@
-const noteList = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
+type AccidentalPreference = 'flats' | 'sharps' | 'most-common';
 
-export const midiNoteNumberToName = (midiNumber: number): string => {
+const noteList: Record<AccidentalPreference, string[]> = {
+  flats: ['C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭', 'A', 'B♭', 'B'],
+  sharps: ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'],
+  'most-common': ['C', 'C♯', 'D', 'E♭', 'E', 'F', 'F♯', 'G', 'A♭', 'A', 'B♭', 'B']
+};
+
+export const midiNoteNumberToName = (midiNumber: number, prefer: AccidentalPreference = 'most-common'): string => {
   const octave = Math.floor(midiNumber / 12) - 1;
   const noteIndex = midiNumber % 12;
-  return noteList[noteIndex] + octave;
+  return noteList[prefer][noteIndex] + octave;
 };
