9079e43b559a8cebdc74ea3635ba468e5a03ba6d
Matt Putnam <matt.s.putnam@gmail.com>
2021-10-15 23:20:22 -0400

    style disabled buttons

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -15,9 +15,9 @@ const StyledButton = styled.button<Pick<Props, 'large' | 'disabled'>>`
   margin: ${({ large }) => (large ? '0 0 0 0.5rem' : '-2px 0 -2px 0.5rem')};
   padding: ${({ large }) => (large ? '0.5rem 0.75rem' : '2px 0.75rem')};
   background-color: ${({ disabled }) => (disabled ? colors.gray[2] : colors.blue[2])};
-  color: inherit;
+  color: ${({ disabled }) => (disabled ? colors.gray[4] : 'white')};
   font-size: unset;
-  border: 1px solid ${({ disabled }) => (disabled ? colors.gray[2] : colors.blue[1])};
+  border: 1px solid ${({ disabled }) => (disabled ? colors.gray[1] : colors.blue[1])};
   border-radius: 3px;
   cursor: ${({ disabled }) => (disabled ? undefined : 'pointer')};
   &:focus {
